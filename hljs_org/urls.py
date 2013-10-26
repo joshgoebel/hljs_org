43c360bc5893f3de56e5ceb4fb12b8365ee7085e
Ivan Sagalaev <maniac@softwaremaniacs.org>
2013-10-26 15:10:12 -0700

    "Contribute" page.
    - used TemplateView generic

--- hljs_org/urls.py
+++ hljs_org/urls.py
@@ -3,12 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.views.generic import TemplateView
+
 from hljs_org import views
 
 urlpatterns = patterns('',
 
     url(r'^$', views.index, name='index'),
     url(r'^download/$', views.download, name='download'),
+    url(r'^contribute/$', TemplateView.as_view(template_name='contribute.html'), name='contribute'),
 
     url(r'^admin/', include(admin.site.urls)),
 
