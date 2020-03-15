70b76d38a393e848e48aef3fcd48b11da05b7aaa
Ivan Sagalaev <maniac@softwaremaniacs.org>
2020-03-14 21:50:52 -0700

    Publish CDN build to npm

--- hljs_org/management/commands/updatehljs.py
+++ hljs_org/management/commands/updatehljs.py
@@ -78,6 +78,11 @@ class Command(BaseCommand):
             run(['git', 'tag', version])
         run(['git', 'push'])
         run(['git', 'push', '--tags'])
+        if os.path.exists('package.json'):
+            log.info('Publishing CDN build to npm...')
+            run(['npm', 'publish', '.'])
+        else:
+            log.info('package.json not found in %s' % settings.HLJS_CDN_SOURCE)
         os.chdir(settings.HLJS_SOURCE)
 
         call_command('publishtest')
